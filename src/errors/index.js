0aeddcb3418e300af8935a7cc2a99827541668cf
Silke Jänichen <sjaenichen@exa-online.de>
2023-06-09 11:56:21 +0200

    Add src files

---
+++ src/errors/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SueljeApiTimeoutError = exports.SueljeApiError = void 0;
+var SueljeApiError_1 = require("./SueljeApiError");
+Object.defineProperty(exports, "SueljeApiError", { enumerable: true, get: function () { return SueljeApiError_1.SueljeApiError; } });
+var SueljeApiTimeoutError_1 = require("./SueljeApiTimeoutError");
+Object.defineProperty(exports, "SueljeApiTimeoutError", { enumerable: true, get: function () { return SueljeApiTimeoutError_1.SueljeApiTimeoutError; } });
